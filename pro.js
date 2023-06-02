bef97c5fa4ceecd4eaf705e75e5361f1c1f65953
I-AnkitKumarSingh <116379479+I-AnkitKumarSingh@users.noreply.github.com>
2023-06-02 13:44:23 +0530

    using java script

---
+++ pro.js
@@ -0,0 +1,21 @@
+click_to_convert.addEventListener('click',function(){
+    var speech = true;
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('')
+
+        document.getElementById("your_text").innerHTML = transcript;
+        console.log(transcript);
+    });
+    
+    if (speech == true) {
+        recognition.start();
+    }
+})
